578893f1df08c325b9e444201195bb98c59a9284
juli <juliju2475@gmail.com>
2019-11-25 21:47:58 +0200

    11test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Create merge/pull request");
+        System.out.println("Yula ");
+        System.out.println("2");
     }
 }
