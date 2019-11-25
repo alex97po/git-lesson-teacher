bc85c2e66736f87206a3f01bc2648676f462bcb3
Vandini <egor96471@gmail.com>
2019-11-25 21:39:31 +0200

    complite 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Juli\n77");
+        System.out.println("Juli");
+        System.out.println("1");
     }
 }
