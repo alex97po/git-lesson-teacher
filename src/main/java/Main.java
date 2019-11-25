e48f5befef8512d3e43719a242fcec02925df0c8
Vasily Sajko <sajk71@gmail.com>
2019-11-25 21:39:48 +0200

    First commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Create merge/pull request");
+    public static void main(String[] args)
+    {
+        System.out.println("Alexander");
+        System.out.println("5");
     }
 }
