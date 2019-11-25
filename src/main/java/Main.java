21da4cdb12ab16f4f7f90bdcb1503c0a8c8d8870
danil <reloaderlev@gmail.com>
2019-11-25 22:10:32 +0200

    Merge test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-    System.out.println("Viktor");
-    System.out.println(3);
+    System.out.println("Viktor5");
+    System.out.println(35);
     }
 }
