86102660f9db52c60d603a2e177dee07188be1cd
Alexander-Mykolyshyn <oleksandr.mykolyshyn@gmail.com>
2019-11-25 21:39:39 +0200

    тащи

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Create merge/pull request");
+        System.out.println("Alexander");
+        System.out.println("8");
     }
 }
