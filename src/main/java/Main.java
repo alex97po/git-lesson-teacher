50abbeea88ac7a6b76231f9a0100651f952e466e
OPogorelov <oleksandr.pohorelov@jevera.software>
2019-11-28 12:27:23 +0200

    Added main method to launch bmi calculation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,18 @@
 public class Main {
     //To launch game
-    public static void main(String[] args) {
-        Game game = new Game();
-        String player1 = game.inputPlayer1();
-        String player2 = game.inputPlayer2();
-        game.calculateWinner(player1, player2);
-    }
+//    public static void main(String[] args) {
+//        Game game = new Game();
+//        String player1 = game.inputPlayer1();
+//        String player2 = game.inputPlayer2();
+//        game.calculateWinner(player1, player2);
+//    }
 
     //To launch BMI
+    public static void main (String[] args) {
+        BMI index = new BMI();
+        double height = index.inputHeight();
+        int weight = index.inputWeight();
+        double bmi = index.calculate(height, weight);
+        index.outputResult(bmi);
+    }
 }
