bd75690c0bd397208c60982130ec7a6fb131be05
danil <reloaderlev@gmail.com>
2019-12-04 19:55:27 +0200

    Scanner input number

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Index index = new index();
-        double height = Index.inputHeight();
-        int weight = Index.inputWeight();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Type your height:");
+        double height = scanner.nextDouble();
+        System.out.println("Your height is:" + height);
     }
 }
+
+  /*
+    public double inputHeight2() {
+        public double height2;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Type your height:");
+        double height2 = scanner.nextDouble();
+        return height2;
+    }
+*/
